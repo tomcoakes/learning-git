7531c2f913f3a4b6b453ade55d5b488d9ded0ffa
Tom Coakes <tomcoakes@gmail.com>
2015-01-08 17:33:28 +0000

    Hello.rb  uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,8 @@
-# Default is "World"
 # Author: Tom Coakes (tomcoakes@gmail.com)
+require 'greeter'
+
+# Default is "World"
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
